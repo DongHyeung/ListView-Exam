2e61bb31ff844f1ee99e6f7470dd05b2d793549c
DongHyeung <ehdgud2781@naver.com>
2015-08-31 14:18:32 +0900

    Custom 어댑터 작성 ListView에 Custom 어댑터

--- app/src/main/java/com/android/example/listviewexam/MainActivity.java
+++ app/src/main/java/com/android/example/listviewexam/MainActivity.java
@@ -4,16 +4,13 @@ package com.android.example.listviewexam;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-    private List<Map<String, String>> mData;
-    private SimpleAdapter mAdapter;
+    private List<People> mData;
+    private CustomAdapter mAdapter;
     private ListView mListView;
 
     @Override
@@ -32,27 +29,21 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void initData() {
-        mData = new ArrayList<>();
-        for (int i = 1; i <= 100; i++) {
-            Map<String, String> data = new HashMap<>();
-            data.put("item", "Item" + i);
-            data.put("subitem", "Sub Item" + i);
-            mData.add(data);
-        }
-    }
-
-    private void initAdapter() {
-        mAdapter = new SimpleAdapter(this, mData, android.R.layout.simple_list_item_2,
-                new String[] {
-                        "item", "subitem"
-                }, new int[] {
-                        android.R.id.text1, android.R.id.text2
-                });
-    }
-
     private void initListView() {
         mListView = (ListView) findViewById(R.id.List_view);
         mListView.setAdapter(mAdapter);
+    }
+
+    public void initAdapter() {
+        mAdapter = new CustomAdapter(getApplicationContext(), mData);
+    }
+
+    private void initData() {
+        mData = new ArrayList<>();
+        mData.add(new People(R.drawable.elsa, "엘사", "01090912781"));
+        mData.add(new People(R.drawable.elsa, "엘사2", "00113121313"));
+        for (int i = 1; i <= 100; i++) {
+            mData.add(new People(R.mipmap.ic_launcher, "아무개" + 1, "번호없음"));
+        }
     }
 }
